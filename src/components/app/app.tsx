8c6613d9e74dfa6d64bf1ce43df874cff47364bf
ArturShchekachPav <117753506+ArturShchekachPav@users.noreply.github.com>
2025-12-27 07:50:12 +0000

    feat: add pages folder

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,6 @@
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { Route, Routes } from 'react-router-dom';
 
 import { AppHeader } from '@components/app-header/app-header';
 import { BurgerConstructor } from '@components/burger-constructor/burger-constructor';
@@ -17,6 +18,15 @@ export const App = (): JSX.Element => {
         Соберите бургер
       </h1>
       <main className={`${styles.main} pl-5 pr-5`}>
+        <Routes location={}>
+          <Route index element="" />
+          <Route path="/login" element="" />
+          <Route path="/register" element="" />
+          <Route path="/forgot-password" element="" />
+          <Route path="/reset-password" element="" />
+          <Route path="/profile" element="" />
+          <Route path="/ingredients/:id" element="" />
+        </Routes>
         <DndProvider backend={HTML5Backend}>
           <BurgerIngredients />
           <BurgerConstructor />
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,25 @@
-export function debounce(func: (...arg: unknown[]) => void, delay: number) {
-  let timeoutId: NodeJS.Timeout;
+// export function debounce(func: (...arg: unknown[]) => void, delay: number) {
+//   let timeoutId: NodeJS.Timeout;
 
-  return (...arg: Parameters<typeof func>): void => {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => func(...arg), delay);
-  };
-}
+//   return (...arg: Parameters<typeof func>): void => {
+//     clearTimeout(timeoutId);
+//     timeoutId = setTimeout(() => func(...arg), delay);
+//   };
+// }
+
+// export function checkResponse(res: Response): unknown {
+//   return res.ok ? res.json() : res.json().then(err => {throw new Error(err)});
+// }
+
+// export function refreshToken(): Promise<Response> {
+//   const requestBody: { token: string } = { token: '' };
+//   const body: string = JSON.stringify(requestBody);
+
+//   return fetch('/auth/token', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body
+//   })
+// }
